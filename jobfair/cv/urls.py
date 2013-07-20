d77fb6057dc35917793f694ddcc359f86598b25e
Tomislav Maricevic <tmarice@gmail.com>
2013-07-20 16:15:20 +0200

    Fixed minor typo bug

--- jobfair/cv/urls.py
+++ jobfair/cv/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^new$', views.new, name='new'),
+        url(r'^new/$', views.new, name='new'),
         url(r'^edit/$', views.edit_page, name='edit page'),
         url(r'^edit/(?P<access_code>\w+)/$', views.edit_profile, name='edit profile'),
         )
