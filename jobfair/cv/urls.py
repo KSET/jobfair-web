17180afc9e93c482ca328d5feff7af524d8079d0
Tomislav Maricevic <tmarice@gmail.com>
2013-07-20 13:28:33 +0200

    Added aditional views and corresponding urls

--- jobfair/cv/urls.py
+++ jobfair/cv/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index')
+        url(r'^$', views.index, name='index'),
+        url(r'^new$', views.new, name='new'),
+        url(r'^edit/$', views.edit_page, name='edit page'),
+        url(r'^edit/(?P<access_code>\w+)/$', views.edit_profile, name='edit profile'),
         )
--- jobfair/cv/views.py
+++ jobfair/cv/views.py
@@ -2,3 +2,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("CV INDEX PAGE")
+
+def new(request):
+    return HttpResponse("NEW CV")
+
+def edit_profile(request, access_code="-1"):
+    return HttpResponse("Editing CV with access code {}".format(access_code))
+
+def edit_page(request):
+    return HttpResponse("Enter your login info for profile editing")
+
