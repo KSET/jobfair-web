c1030e54e8b2b7c2f62f5eb85cefedaa5116cf22
Tomislav Maricevic <tmarice@gmail.com>
2013-07-24 16:58:15 +0200

    Unified edit urls

--- jobfair/cv/urls.py
+++ jobfair/cv/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^new/$', views.new, name='new'),
-        url(r'^edit/$', views.edit_page, name='edit page'),
-        url(r'^edit/(?P<access_code>\w+)/$', views.edit_profile, name='edit profile'),
+        url(r'^edit/$', views.edit, name='edit'),
         )
