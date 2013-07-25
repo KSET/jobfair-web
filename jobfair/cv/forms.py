63723885bff5798e60c1f5b74c7b47ab113669f2
Tomislav Maricevic <tmarice@gmail.com>
2013-07-25 16:02:43 +0200

    Added support for multiple form(set) on one page

--- jobfair/cv/forms.py
+++ jobfair/cv/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm, ValidationError
 from django.contrib.auth.models import User as AuthUser
+from django.forms.formsets import formset_factory, BaseFormSet
 
 import models
 
@@ -18,23 +19,35 @@ class UserDetailForm(ModelForm):
 class EducationForm(ModelForm):
     class Meta:
         model = models.Education
+        exclude = ('user',)
 
 
 class ForeignLanguageForm(ModelForm):
     class Meta:
         model = models.ForeignLanguage
+        exclude = ('user',)
 
 
 class ExperienceForm(ModelForm):
     class Meta:
         model = models.Experience
+        exclude = ('user',)
 
 
 class SkillsForm(ModelForm):
     class Meta:
         model = models.Skills
+        exclude = ('user',)
 
 
 class OtherForm(ModelForm):
     class Meta:
         model = models.Other
+        exclude = ('user',)
+
+
+educationFormSet = formset_factory(EducationForm, max_num=10)
+foreignLanguageFormSet = formset_factory(ForeignLanguageForm, max_num=10)
+experienceFormSet = formset_factory(ExperienceForm, max_num=10)
+skillsFormSet = formset_factory(SkillsForm, max_num=30)
+
--- jobfair/cv/views.py
+++ jobfair/cv/views.py
@@ -14,29 +14,37 @@ def new(request):
     context = dict()
     userForm = forms.UserForm(request.POST or None)
     userDetailForm = forms.UserDetailForm(request.POST or None)
-    educationForm = [forms.EducationForm(request.POST or None)]
-    foreignLanguageFrom = [forms.ForeignLanguageForm(request.POST or None)]
-    experienceForm = [forms.ExperienceForm(request.POST or None)]
-    skillsForm = [forms.SkillsForm(request.POST or None)]
+    educationFormSet = forms.educationFormSet(request.POST or None)
+    foreignLanguageFormSet = forms.foreignLanguageFormSet(
+            request.POST or None)
+    experienceFormSet = forms.experienceFormSet(request.POST or None)
+    skillsFormSet = forms.skillsFormSet(request.POST or None)
     otherForm = forms.OtherForm(request.POST or None)
-    context['user_form'] = userForm
-    context['user_detail_form'] = userDetailForm
-    context['education_forms'] = educationForm
-    context['foreign_language_forms'] = foreignLanguageFrom
-    context['experience_forms'] = experienceForm
-    context['skills_forms'] = skillsForm
-    context['other_form'] = otherForm
-    if userForm.is_valid() and userDetailForm.is_valid():
-        new_user = userForm.save()
-        new_user_detail = userDetailForm.save(commit=False)
-        new_user_detail.user = new_user
-        new_user_detail.save()
+    context = {'user_form': userForm,
+            'user_detail_form': userDetailForm,
+            'education_formset': educationFormSet,
+            'foreign_language_formset': foreignLanguageFormSet,
+            'experience_formset': experienceFormSet,
+            'skills_formset': skillsFormSet,
+            'other_form': otherForm,
+            }
+    valid_list = map(lambda x: x.is_valid(), context.itervalues())
+    if all(valid_list):
+        user = userForm.save()
+        user_detail = userDetailForm.save(commit=False)
+        user_detail.user = user
+        user_detail.save()
+        for form in educationFormSet:
+            education = form.save(commit=False)
+            education.user = user
+            education.save()
+
         context['message'] = 'Thank you, your input has been saved.'
     return render(request, 'cv/new.html', context)
 
 def edit(request):
     if request.user.is_authenticated():
-        return HttpResponse("Editing CV of a user {}, password {}".format(
-            request.user.username, request.user.password))
+        return HttpResponse("Editing CV of a user {}".format(
+            request.user.username))
     return HttpResponse("Need to log in")
 
